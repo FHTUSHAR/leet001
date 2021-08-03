326d131468a64209d991a84c2c4fa8dbf2d7e565
Md.Forhud Hossain Tushar <53527970+FHTUSHAR@users.noreply.github.com>
2021-08-03 14:10:45 +0600

    Add files via upload

---
+++ leet128.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str)->bool:
+        a=''
+        for i in s:
+           if i.isalnum():
+               a+=i
+        
+        a=a.lower()
+        print(a)
+        start=0
+        end=len(a)-1
+        while start<end:
+            if a[start]!=a[end]:
+                return False
+            start+=1
+            end-=1
+        return True
+
+
+p=Solution()
+s = "A man, a plan, a canal: Panama"
+print(p.isPalindrome(s))
